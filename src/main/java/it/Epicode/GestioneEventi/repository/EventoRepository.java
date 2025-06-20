85dbac94d69cb4560ad4b9ecd4f6da1df5f24a0a
MicheleFamoso <michelefamoso@gmail.com>
2025-06-20 17:35:54 +0200

    a

--- src/main/java/it/Epicode/GestioneEventi/repository/EventoRepository.java
+++ src/main/java/it/Epicode/GestioneEventi/repository/EventoRepository.java
@@ -1,7 +1,12 @@
 package it.Epicode.GestioneEventi.repository;
 
 import it.Epicode.GestioneEventi.model.Evento;
+import it.Epicode.GestioneEventi.model.Utente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventoRepository extends JpaRepository<Evento,Integer> {
+
+    List<Evento> findByOrganizzatore(Utente organizzatore);
 }
