41e28cc5fa9fa1f036494a6d0c4a2c225bd0306e
MicheleFamoso <michelefamoso@gmail.com>
2025-06-20 15:14:54 +0200

    Add Utente

--- src/main/java/it/Epicode/GestioneEventi/model/Utente.java
+++ src/main/java/it/Epicode/GestioneEventi/model/Utente.java
@@ -1,4 +1,54 @@
 package it.Epicode.GestioneEventi.model;
 
-public class Utente {
+import it.Epicode.GestioneEventi.enumeration.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Data
+public class Utente implements UserDetails {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nome;
+    private String cognome;
+    @Column(unique = true)
+    private String username;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+private List<Evento> eventiPrenotati;
+private List<Evento> eventiCreati;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
